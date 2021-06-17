af2bac68f009c438fb732abf6a9ea1edae0d94a1
Prahart30 <63900522+Prahart30@users.noreply.github.com>
2021-06-17 20:07:41 +0530

    Update test_mode.c

--- test_mode.c
+++ test_mode.c
@@ -2,7 +2,25 @@
 #include <assert.h>
 
 
-int mode(int [], int);
+int mode(int a[],int n) {
+   int maxValue = 0, maxCount = 0, i, j;
+
+   for (i = 0; i < n; ++i) {
+      int count = 0;
+      
+      for (j = 0; j < n; ++j) {
+         if (a[j] == a[i])
+         ++count;
+      }
+      
+      if (count > maxCount) {
+         maxCount = count;
+         maxValue = a[i];
+      }
+   }
+
+   return maxValue;
+}
 
 int main(void) {
   int x[] = {4, 9,5,6,5,10,0,2,-3, -3,4, 4};
