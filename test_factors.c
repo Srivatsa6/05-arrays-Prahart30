c8898c289e22d62f272b464b72a4189df8f35ce1
Prahart30 <63900522+Prahart30@users.noreply.github.com>
2021-06-17 20:48:28 +0530

    Update test_factors.c

--- test_factors.c
+++ test_factors.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <assert.h>
 
-int factors(int a[], int n){
-        int i=0, j;
+int factors(int n,int e[]){
+        int i=0;
         while(n%2==0){
-          a[i]=2;
+          e[i]=2;
           i++;
           n=n/2;
         }
         for(int j=3; j<=n;j++){
           while(n%j==0){
-            a[i]=j;
+            e[i]=j;
             i++;
             n=n/j;
           }
         }
         if(n>2)
-          a[i]=n;
+          e[i]=n;
         return i;
       }
 
