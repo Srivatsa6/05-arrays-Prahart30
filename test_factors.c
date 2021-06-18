26614b90fdd6aab6382ee3684e7a86ea8a2e2cc5
Prahart30 <63900522+Prahart30@users.noreply.github.com>
2021-06-18 09:01:56 +0530

    Update test_factors.c

--- test_factors.c
+++ test_factors.c
@@ -1,25 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
 
-int factors(int , int []);
-
-int main(void) {
-  int ret[100] = {0};
-  int count = factors(180, ret);
-  assert (count == 5);
-  assert (ret[0] == 2);
-  assert (ret[1] == 2);
-  assert (ret[2] == 3);
-  assert (ret[3] == 3);
-  assert (ret[4] == 5);
-
-
-  count = factors(143, ret);
-  assert (count == 2);
-  assert (ret[0] == 11);
-  assert (ret[1] == 13);
-  printf("Factors: passed\n");
-}
    int factors(int n,int e[]){
         int i=0, j;
         while(n%2==0){
@@ -38,3 +19,23 @@ int main(void) {
           e[i]=n;
         return i;
       }
+
+int main(void) {
+  int ret[100] = {0};
+  int count = factors(180, ret);
+  assert (count == 5);
+  assert (ret[0] == 2);
+  assert (ret[1] == 2);
+  assert (ret[2] == 3);
+  assert (ret[3] == 3);
+  assert (ret[4] == 5);
+
+
+  count = factors(143, ret);
+  assert (count == 2);
+  assert (ret[0] == 11);
+  assert (ret[1] == 13);
+  printf("Factors: passed\n");
+}
+
+        
